c291800d6cc2e70ac3e5a70382a465fe8e5368ff
Wesley Fisher <wfisher@praetor.tel>
2024-07-29 14:45:33 -0500

    libvirt

--- modules/virtualization/default.nix
+++ modules/virtualization/default.nix
@@ -23,4 +23,20 @@
     docker-compose # start group of containers for dev
     buildah
   ];
+
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [(pkgs.OVMF.override {
+          secureBoot = true;
+          tpmSupport = true;
+        }).fd];
+      };
+    };
+  };
 }
