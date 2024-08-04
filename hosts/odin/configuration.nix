2cb95fad8e422a35fde29687361e50932b9011ee
Wesley Fisher <wfisher@praetor.tel>
2024-08-03 21:21:58 -0500

    checkin

--- hosts/odin/configuration.nix
+++ hosts/odin/configuration.nix
@@ -53,7 +53,15 @@ i18n.extraLocaleSettings = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
-    pulse.enable = true;
+    jack.enable = true;
+  };
+  services.pipewire.wireplumber.extraConfig.bluetoothEnhancements = {
+    "monitor.bluez.properties" = {
+      "bluez5.enable-sbc-xq" = true;
+      "bluez5.enable-msbc" = true;
+      "bluez5.enable-hw-volume" = true;
+      "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
+    };
   };
 
   nixpkgs.config.allowUnfree = true;
