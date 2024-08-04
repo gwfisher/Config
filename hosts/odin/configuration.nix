ec0decf5404fcb4f2ba91814c2fdc73026cd38f0
Wesley Fisher <wfisher@praetor.tel>
2024-08-03 21:22:55 -0500

    sound shit

--- hosts/odin/configuration.nix
+++ hosts/odin/configuration.nix
@@ -49,20 +49,6 @@ i18n.extraLocaleSettings = {
   services.tailscale.enable = true;
 
   security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    jack.enable = true;
-  };
-  services.pipewire.wireplumber.extraConfig.bluetoothEnhancements = {
-    "monitor.bluez.properties" = {
-      "bluez5.enable-sbc-xq" = true;
-      "bluez5.enable-msbc" = true;
-      "bluez5.enable-hw-volume" = true;
-      "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
-    };
-  };
 
   nixpkgs.config.allowUnfree = true;
 
